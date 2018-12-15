5fb78cffc3fe37e929e313ce97cae441d82efdcf
Abdelrahmam Salem <abubakir1997@gmail.com>
2018-12-15 13:51:05 -0500

    Updating listeners API

--- src/notifications/notifications.listeners.js
+++ src/notifications/notifications.listeners.js
@@ -1,15 +1,11 @@
 import { reducePositions } from './notifications.shared'
 
 export const persist = {
-    rehydrate: store => {
-        const { positions } = store.get()
-
-        return {
-            open: false,
-            clearing: false,
-            positions: reducePositions(positions, ({ notifications }) => ({
-                notifications: notifications.filter(notification => notification.open),
-            })),
-        }
-    },
+    rehydrate: () => ({ positions }) => ({
+        open: false,
+        clearing: false,
+        positions: reducePositions(positions, ({ notifications }) => ({
+            notifications: notifications.filter(notification => notification.open),
+        })),
+    }),
 }
