15bc9fb2deacec70566b756609eccecf26170994
Abdelrahmam Salem <abubakir1997@gmail.com>
2018-12-18 11:51:54 -0500

    Adding logger flag

--- src/logger/index.js
+++ src/logger/index.js
@@ -3,6 +3,8 @@ export default (settings = {}) => store => {
 
     if (process.env.NODE_ENV !== 'production' || production) {
         store.subscribe((action, args) => {
+            if (window.logger === false) return
+
             console.group(action)
             console.log(`args:`, args)
             console.log(`state:`, store.get())
