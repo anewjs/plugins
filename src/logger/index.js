a3d296a8a5900833f464343b62698e8f1b439bd9
Abdelrahmam Salem <abubakir1997@gmail.com>
2019-02-03 11:32:22 -0500

    Display before/after state in logger

--- src/logger/index.js
+++ src/logger/index.js
@@ -2,12 +2,15 @@ export default (settings = {}) => store => {
     const { production } = settings
 
     if (process.env.NODE_ENV !== 'production' || production) {
+        let prevState = {}
+
         store.subscribe((action, args) => {
             if (window.logger === false || localStorage.getItem('logger') === 'false') return
 
             console.group(action)
             console.log(`args:`, args)
-            console.log(`state:`, store.get())
+            console.log(`before:`, prevState)
+            console.log(`after:`, (prevState = JSON.parse(JSON.stringify(store.get()))))
             console.groupEnd(action)
         })
     }
