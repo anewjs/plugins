fe4e12ace1f99355d04b10bda448b62674915f67
Abdelrahmam Salem <abubakir1997@gmail.com>
2019-02-06 18:24:14 -0500

    Adding router get method to router store

--- src/router/index.js
+++ src/router/index.js
@@ -83,6 +83,7 @@ export default ({ history, router } = {}) => (store, options) => {
                     search: state => state.location.search,
                     state: state => state.location.state,
                     key: state => state.location.key,
+                    get: name => router.get(name),
                 },
 
                 selectors: {
