b0bda25b566d633858132670b7e2c28167b3cd6a
Abdelrahman Salem (Dell) <abdelrahmansalem@boomi.com>
2019-11-04 14:59:52 -0500

    Updating getters plugin

--- src/getters/index.js
+++ src/getters/index.js
@@ -1,17 +1,30 @@
 export default (store, options) => {
-    options.inject({
-        modules: Object.entries(modules).reduce((modules, [storeName, store]) => {
-            modules[storeName] = {
-                ...store,
-                getters: {
-                    ...Object.keys(store.state).reduce((getters, statePropKey) => {
-                        getters[statePropKey] = state => state[statePropKey]
+    const { modules = {}, state = {} } = options.get()
 
-                        return getters
-                    }, {}),
-                    ...store.getters,
-                },
-            }
+    options.inject({
+        getters: {
+            ...Object.keys(state).reduce((getters, statePropKey) => {
+                getters[statePropKey] = state => state[statePropKey]
+
+                return getters
+            }, {}),
+            ...store.getters,
+        },
+        modules: Object.entries(modules).reduce((modules, [storeName, store]) => {
+            modules[storeName] =
+                typeof store.state === 'object'
+                    ? {
+                          ...store,
+                          getters: {
+                              ...Object.keys(store.state).reduce((getters, statePropKey) => {
+                                  getters[statePropKey] = state => state[statePropKey]
+
+                                  return getters
+                              }, {}),
+                              ...store.getters,
+                          },
+                      }
+                    : store
 
             return modules
         }, {}),
