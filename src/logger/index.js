8b79336cd33f874f028dd09bd0e60e7376e04009
Abdelrahmam Salem <abubakir1997@gmail.com>
2019-11-23 17:53:50 -0500

    Updating logger with new @anew/store api

--- src/logger/index.js
+++ src/logger/index.js
@@ -32,14 +32,15 @@ export default ({ production, stringifier = new Stringifier() } = {}) => store =
     if (process.env.NODE_ENV !== 'production' || production) {
         let prevState = {}
 
-        store.subscribe((action, args) => {
+        store.subscribe((action, args, callType) => {
             if (window.logger === false || localStorage.getItem('logger') === 'false') return
+            const groupId = `<${callType}> ${action}`
 
-            console.group(action)
+            console.group(groupId)
             console.log(`args:`, args)
             console.log(`before:`, prevState)
             console.log(`after:`, (prevState = JSON.parse(stringifier.stringify(store.get()))))
-            console.groupEnd(action)
+            console.groupEnd(groupId)
         })
     }
 }
